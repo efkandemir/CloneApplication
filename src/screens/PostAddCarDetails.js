2b319542209179af8805b0665948b95ade3a870f
Efkan Demir <demirefkan33@gmail.com>
2025-04-30 08:55:18 +0300

    PostAdd page edited

--- src/screens/PostAddCarDetails.js
+++ src/screens/PostAddCarDetails.js
@@ -9,108 +9,175 @@ import {
 const PostAddCarDetails = ({ route }) => {
   const { year } = route.params;
   const { marka } = route.params;
+
   return (
-    <View className="flex-1 bg-white">
-      <View className="w-full h-8 bg-sahibindenblur items-center justify-center">
-        <Text>
+    <View className="flex-1 bg-sahibindengray">
+      <View className="w-full h-9 bg-white items-center justify-center flex-row px-2 border-b border-gray-200">
+        <Text className="text-base">
           Vasıta {">"} Otomobil {">"} {year} {">"} {marka}
         </Text>
       </View>
-      <ScrollView className="px-4">
-        <View className="my-3">
-          <Text className="text-gray-700 font-semibold mb-2">İlan Başlığı</Text>
+
+      <ScrollView className="px-4 bg-white ">
+        <View className="bg-white">
+          <Text className="text-lg font-bold text-gray-800 mb-2">
+            XXX 1.y {marka} el arabası
+          </Text>
+
+          <View className="flex-row flex-wrap mb-2 border-b border-gray-200">
+            <Text className="text-black text-sm mr-2">• Dizel</Text>
+            <Text className="text-black text-sm mr-2">• Camlı Van 5 kapı</Text>
+            <Text className="text-black text-sm mr-2">• 225 HP</Text>
+            <Text className="text-black text-sm mr-2">•{year}</Text>
+          </View>
+          <View className="flex-row items-center my-1  border-b border-gray-200 pb-3">
+            <Text className="text-black font-semibold w-1/3">Vites</Text>
+            <TextInput className="p-1 ml-2 " placeholder="Manuel" multiline />
+          </View>
+          <View className="flex-row items-center my-1  border-b border-gray-200 pb-3">
+            <Text className="text-black font-semibold w-1/3">Kasa Tipi</Text>
+            <TextInput
+              className="p-1 ml-2 "
+              placeholder="Camlı Van"
+              multiline
+            />
+          </View>
+          <View className="flex-row items-center my-1  border-b border-gray-200 pb-3">
+            <Text className="text-black font-semibold w-1/3">Çekiş</Text>
+            <TextInput
+              className="p-1 ml-2 "
+              placeholder="4x2 (Önden Çekişli)"
+              multiline
+            />
+          </View>
+        </View>
+
+        <View className="flex-row items-center my-3 border-b border-gray-200 pb-1 mt-4">
+          <Text className="text-black font-semibold w-1/3">İlan Başlığı</Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3"
+            className="flex-1  rounded-md p-1 ml-2"
             placeholder="Başlık Girin"
           />
         </View>
 
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">Açıklama</Text>
+        <View className="flex-row items-center my-1  border-b border-gray-200 pb-3">
+          <Text className="text-black font-semibold w-1/3">Açıklama</Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3 h-32 text-start"
+            className="p-1 ml-2 "
             placeholder="Açıklama Girin"
             multiline
           />
         </View>
 
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">Fiyat</Text>
+        <View className="flex-row items-center my-3  pb-3 border-b border-gray-200">
+          <Text className="text-black font-semibold w-1/3">Fiyat</Text>
+          <View className="flex-1 flex-row items-center ml-2">
+            <TextInput
+              className="flex-1  rounded-md p-1"
+              placeholder="Fiyat Girin"
+              keyboardType="numeric"
+            />
+            <Text className="ml-2 text-black">TL</Text>
+          </View>
+        </View>
+
+        <View>
+          <Text className="text-black font-bold text-base">Araç Durumu</Text>
+        </View>
+
+        <View className="flex-row items-center my-3">
+          <Text className="text-black font-semibold w-1/3">Km</Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3"
-            placeholder="Fiyat Girin"
+            className="flex-1  rounded-md p-1 ml-2"
+            placeholder="Km Girin"
             keyboardType="numeric"
           />
         </View>
 
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">Araç Durumu</Text>
+        <View className="flex-row items-center my-3">
+          <Text className="text-black font-semibold w-1/3">Şasi</Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3"
-            placeholder="Araç Durumu Girin"
-          />
-        </View>
-
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">Km</Text>
-          <TextInput
-            className="border border-gray-300 rounded-xl p-3"
-            placeholder="Kilometre Girin"
-            keyboardType="numeric"
-          />
-        </View>
-
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">Şasi</Text>
-          <TextInput
-            className="border border-gray-300 rounded-xl p-3"
+            className="flex-1  rounded-md p-1 ml-2"
             placeholder="Şasi Numarası Girin"
           />
         </View>
 
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">
-            Koltuk Sayısı
-          </Text>
+        <View className="flex-row items-center my-3">
+          <Text className="text-black  font-semibold w-1/3">Koltuk Sayısı</Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3"
-            placeholder="Koltuk Sayısı Girin"
+            className="flex-1  rounded-md p-1 ml-2"
+            placeholder="4+1"
+            value="4+1"
           />
         </View>
 
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">Renk</Text>
+        <View className="flex-row items-center my-3">
+          <Text className="text-black font-semibold w-1/3">Renk</Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3"
+            className="flex-1  rounded-md p-1 ml-2"
             placeholder="Renk Girin"
           />
         </View>
 
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">Ruhsat Kaydı</Text>
+        <View className="flex-row items-center my-3">
+          <Text className="text-black font-semibold w-1/3">Ruhsat Kaydı</Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3"
+            className="flex-1  rounded-md p-1 ml-2"
             placeholder="Ruhsat Kaydı Girin"
           />
         </View>
 
-        <View className="my-4">
-          <Text className="text-gray-700 font-semibold mb-2">
+        <View className="flex-row items-center my-3 mb-6">
+          <Text className="text-black font-semibold w-1/3">
             Ağır Hasar Kaydı
           </Text>
           <TextInput
-            className="border border-gray-300 rounded-xl p-3"
-            placeholder="Ağır Hasar Kaydı Var mı?"
+            className="flex-1  rounded-md p-1 ml-2"
+            placeholder="Var/Yok"
           />
         </View>
 
-        <TouchableOpacity className="bg-blue-600 rounded-xl p-4 mt-6 mb-10">
-          <Text className="text-white text-center font-semibold text-lg">
-            Devam Et
+        <View className="mb-4">
+          <Text className="text-black font-bold text-sm">Araç Plakası</Text>
+        </View>
+
+        <View className="flex-row items-center my-1">
+          <Text className="text-gray-700 font-semibold w-1/3">
+            Araç Plakası
           </Text>
-        </TouchableOpacity>
+          <TextInput
+            className="flex-1  rounded-md p-1 ml-2"
+            placeholder="Plakayı girin"
+          />
+        </View>
+
+        <View>
+          <Text className="text-gray-500 text-sm">
+            Araç plakası yayınlanan ilanlarda görünmeyecektir.
+          </Text>
+        </View>
+
+        <View className="flex-row items-center my-5">
+          <Text className="text-gray-700 font-semibold w-1/3">Takas</Text>
+          <View className="flex-1 ml-2">
+            <Text className="text-gray-700">Evet/Hayır seçenekleri buraya</Text>
+          </View>
+        </View>
+
+        <View className="flex-row items-center my-3 mb-8">
+          <Text className="text-gray-700 font-semibold w-1/3">Plaka Türü</Text>
+          <View className="flex-1 ml-2">
+            <Text className="text-gray-700">
+              Türkiye/Yabancı plaka seçenekleri
+            </Text>
+          </View>
+        </View>
       </ScrollView>
+      <TouchableOpacity className="bg-blue-600 rounded-lg p-3  ">
+        <Text className="text-white text-center font-semibold">Devam Et</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
 export default PostAddCarDetails;
